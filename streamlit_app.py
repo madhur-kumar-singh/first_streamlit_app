a698919706989061eaa958bfb1f1859a5fc0f86e
Madhur Kumar Singh <madhursingh04072@gmail.com>
2023-02-07 15:02:18 +0530

    Update streamlit_app.py
    Data Changes Updated.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,16 @@
 import streamlit
-streamlit.title("Hello")
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.title("My Parent New Healthy Diner")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+
+
