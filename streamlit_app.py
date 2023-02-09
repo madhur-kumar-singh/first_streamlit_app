c4a71bfc05a94e1c614cf627efdcb8501dd276d9
Madhur Kumar Singh <madhursingh04072@gmail.com>
2023-02-09 11:38:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if streamlit.button('Get fruit load List'):
   streamlit.dataframe(my_data_rows)
 
 
-
+streamlit.stop()
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
